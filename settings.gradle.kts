aa0a31e3bd7ac8d751add60d13fc6a98acf1eef7
samAricha <samaricha001@gmail.com>
2024-01-14 17:43:31 +0300

    successfully running the desktop app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,14 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
 rootProject.name = "Organiks1"
 include(":android")
 include(":desktop")
--- shared/src/commonMain/kotlin/com/joelkanyi/focusbloom/server/Greeting.kt
+++
@@ -1,5 +0,0 @@
-class Greeting {
-    fun greet(): String {
-        return "Hello, from KMP!"
-    }
-}
